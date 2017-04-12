e0d866c0739fb88608155833f7bc912ea8b1939a
Viktoriia Bilous <vikabelous@gmail.com>
2017-04-12 19:27:46 +0300

    LVRUBYM-209: Add constants

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,12 @@
 var angular = require('angular');
 var mainModule = require('./main/main.module.js');
+var constantsModule = require('./common/constants.js');
 var vendorModule = require('./requirements.js');
 
 angular
   .module('schoolArea', [
     vendorModule.name,
+    constantsModule.name,
     mainModule.name
   ])
   .config(function($locationProvider, localStorageServiceProvider) {
---
+++ app/js/common/constants.js
@@ -0,0 +1,6 @@
+module.exports = angular
+  .module('constants', [])
+  .constant('constants', {
+    serverUrl: 'http://localhost:3000',
+    serverUrlV1: 'http://localhost:3000/v1'
+  });
