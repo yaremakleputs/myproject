3022924579ba3a571d33dbb825ed444d04fac39f
Viktoriia Bilous <vikabelous@gmail.com>
2017-04-14 01:26:26 +0300

    LVRUBYM-209: Add logout

--- app/js/main/components/main.component.js
+++ app/js/main/components/main.component.js
@@ -1,11 +1,21 @@
+var authService = require('./../../common/services/auth.service.js');
+
 module.exports = angular
-  .module('main.component', [])
+  .module('main.component', [
+    authService.name
+  ])
   .component('mainComponent', {
     templateUrl: './app/js/main/components/main.template.html',
     controller: MainController
   });
 
-function MainController() {
+MainController.$inject = ['auth'];
+
+function MainController(auth) {
   var ctrl = this;
+
+  ctrl.logout = function() {
+    auth.logout();
+  };
 };
 
