a2b85bf1f3f736604c762f0ea021aca4133a4124
Viktoriia Bilous <vikabelous@gmail.com>
2017-04-14 01:19:35 +0300

    LVRUBYM-209: Add angular-resource

--- app/js/requirements.js
+++ app/js/requirements.js
@@ -1,6 +1,7 @@
 module.exports = angular
   .module('schoolArea.vendor', [
     require('angular-ui-router'),
+    require('angular-resource'),
     require('angular-material'),
     require('angular-local-storage')
     ]);
