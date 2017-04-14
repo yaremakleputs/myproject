7544c6317cc68816cc943f0e444c6bb36eaf95a3
andriyarterchuk <andriyarterchuk@gmail.com>
2017-04-14 13:45:26 +0300

    LVRUBYM-242:deleted console.log, changed one module name, factory name ,formatted  main.template.html

---
+++ app/js/common/resources/my_day_report.resource.js
@@ -0,0 +1,15 @@
+var constants = require('./../constants');
+module.exports = angular
+  .module('myDayReport.resource', [
+    constants.name
+  ])
+  .factory('myDayReportResource', myDayReportResource);
+
+myDayReportResource.$inject = ['$resource', 'globalSettings'];
+
+function myDayReportResource($resource, globalSettings) {
+  return $resource(globalSettings.SERVER_URL_V1 + '/groups/1/my_day_reports/:id.json', {id: '@id'},
+    {
+      'update': {method: 'PUT'}
+    });
+}
