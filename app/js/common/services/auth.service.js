10c7c2a5c5507371b25acc847e58584225c24f00
Viktoriia Bilous <vikabelous@gmail.com>
2017-04-15 16:11:52 +0300

    LVRUBYM-209: Add toggle message for auth errors

--- app/js/common/services/auth.service.js
+++ app/js/common/services/auth.service.js
@@ -7,22 +7,33 @@ module.exports = angular
   .factory('auth', AuthFactory);
 
 AuthFactory.$inject = [
+  '$q',
   '$state',
+  '$mdToast',
   'localStorageService',
   'globalSettings',
+  'errorMessages',
   'authResource'
 ];
 
-function AuthFactory($state, localStorageService, globalSettings, authResource) {
+function AuthFactory(
+  $q,
+  $state,
+  $mdToast,
+  localStorageService,
+  globalSettings,
+  errorMessages,
+  authResource) {
   var toState;
   var fromState;
   var savedState = globalSettings.MAIN_STATE;
-  var authenticated = localStorageService.get('token') ? true : false;
+  var authenticated = !!localStorageService.get('token');
 
   var service = {
     authorization: function(event, to, from) {
       toState = to.name ? to : globalSettings.MAIN_STATE;
       fromState = from.name ? from : globalSettings.MAIN_STATE;
+
       if (toState.skipAuth) {
         if (authenticated) {
           event.preventDefault();
@@ -44,16 +55,19 @@ function AuthFactory($state, localStorageService, globalSettings, authResource) 
         function(response) {
           service.saveToken(response);
           $state.go(savedState);
+        },
+        function(response) {
+          service.toggleErrorMsg(response);
         });
     },
 
     refreshToken: function() {
-      authResource.refreshToken().$promise.then(service.saveToken);
-    },
-
-    logout: function() {
-      savedState = globalSettings.MAIN_STATE;
-      service.removeToken();
+      authResource.refreshToken().$promise.then(
+        service.saveToken,
+        function(response) {
+          service.toggleErrorMsg(response);
+          service.removeToken();
+        });
     },
 
     saveToken: function(response) {
@@ -61,10 +75,35 @@ function AuthFactory($state, localStorageService, globalSettings, authResource) 
       localStorageService.set('token', response.headers()['auth-token']);
     },
 
+    logout: function() {
+      savedState = globalSettings.MAIN_STATE;
+      service.removeToken();
+    },
+
     removeToken: function() {
       authenticated = false;
       localStorageService.remove('token');
       $state.go(globalSettings.LOGIN_STATE);
+    },
+
+    getErrorMsg: function(status) {
+      switch (status) {
+        case 401:
+          return errorMessages.NO_AUTH;
+        case 419:
+          return errorMessages.AUTH_TIMEOUT;
+      }
+    },
+
+    toggleErrorMsg: function(response) {
+      var msg = service.getErrorMsg(response.status);
+
+      $mdToast.show({
+        template: '<md-toast><div class="md-toast-content">' +
+                    msg +
+                  '</div></md-toast>',
+        position: 'top right'
+      });
     }
   };
 
