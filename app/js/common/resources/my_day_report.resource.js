0bd1bc27ce311d3081223c58ee13936059b1d358
andriyarterchuk <andriyarterchuk@gmail.com>
2017-04-16 22:47:51 +0300

    LVRUBYM-330:removed require constants from resource

--- app/js/common/resources/my_day_report.resource.js
+++ app/js/common/resources/my_day_report.resource.js
@@ -1,15 +1,12 @@
-var constants = require('./../constants');
-
 module.exports = angular
-  .module('myDayReport.resource', [
-    constants.name
-  ])
+  .module('myDayReport.resource', [])
   .factory('myDayReportResource', myDayReportResource);
 
 myDayReportResource.$inject = ['$resource', 'globalSettings'];
 
 function myDayReportResource($resource, globalSettings) {
-  return $resource(globalSettings.SERVER_URL_V1 + '/groups/1/my_day_reports/:id.json', {id: '@id'},
+  return $resource(globalSettings.SERVER_URL_V1 + '/groups/:group_id/my_day_reports/:id.json',
+    {group_id: 1, id: '@id'},
     {
       'update': {method: 'PUT'}
     });
