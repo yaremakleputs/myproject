82e7a24a16b4d39d77711d0ba7ba4e1b612e4489
Vitalia <vitalia.durnyak@gmail.com>
2017-05-09 10:33:52 +0300

    LVRUBYM-221:Changed template

--- app/js/static/static.route.js
+++ app/js/static/static.route.js
@@ -1,5 +1,6 @@
 module.exports = angular
   .module('static.route', [])
+<<<<<<< 8e755c941e1599e7941a3fe118b61f581682a2f1
   .config(['$stateProvider',
     function($stateProvider) {
       $stateProvider.state('main.static', {
@@ -9,3 +10,12 @@ module.exports = angular
       });
     }]);
 
+=======
+  .config(function($stateProvider) {
+    $stateProvider.state('main.static', {
+      url: '/static',
+      name: 'main.static',
+      templateUrl: './app/js/static/components/static.template.html'
+    });
+  });
+>>>>>>> LVRUBYM-221:Changed template
