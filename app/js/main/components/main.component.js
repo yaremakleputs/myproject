57640f53789a0b1eda8a42dad333a0d306be095b
hmaherovska <maher.olena96@gmail.com>
2017-05-10 23:06:36 +0300

    LVRUBYM-349:Deleted callendar

--- app/js/main/components/main.component.js
+++ app/js/main/components/main.component.js
@@ -23,19 +23,6 @@ function MainController($scope, $state, auth, currentGroupDay) {
   var ctrl = this;
 
   ctrl.currentGroupDay = currentGroupDay;
-  ctrl.selectedDate = new Date();
-  ctrl.monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
-  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  ctrl.dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-  ctrl.nextDate = function() {
-    ctrl.selectedDate.setDate(ctrl.selectedDate.getDate() + 1);
-  };
-
-  ctrl.pastDate = function() {
-    ctrl.selectedDate.setDate(ctrl.selectedDate.getDate() - 1);
-  };
-
   ctrl.logout = function() {
     auth.logout();
     localStorage.removeItem('activeMenu');
