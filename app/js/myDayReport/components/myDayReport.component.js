a4185d0da52172b9c57d02043e2b80204a4d9e67
andriyarterchuk <andriyarterchuk@gmail.com>
2017-05-17 21:20:10 +0300

    LVRUBYM-242:added changes to myDayReport

--- app/js/myDayReport/components/myDayReport.component.js
+++ app/js/myDayReport/components/myDayReport.component.js
@@ -16,16 +16,12 @@ function MyDayReportController(MyDayReport) {
   MyDayReport.getReports().then(
     function(data) {
       ctrl.students = data;
-    }, function(errors) {
-        return errors;
-      });
+    });
 
   ctrl.myDayReportUpdate = function(student) {
     MyDayReport.updateReports(student.note, student.id)
       .then(function(note) {
       return note;
-    }, function(errors) {
-        return errors;
-      });
+    });
   };
 }
