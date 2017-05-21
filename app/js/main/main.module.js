skrypnyk88/school_area_client

LVRUBYM-220: Add common folder

app/js/main/main.module.js
@@ -4,6 +4,8 @@ var loginModule = require('./../login/login.module.js');
 var presenceReportModule = require('./../presenceReport/presenceReport.module.js');
 var myDayReportModule = require('./../myDayReport/myDayReport.module.js');
 var studentModule = require('./../student/student.module.js');
+var healthReportModule = require('./../healthReport/healthReport.module.js');
+
 require('../../css/style.scss');
 
 module.exports = angular
@@ -13,7 +15,9 @@ module.exports = angular
     loginModule.name,
     presenceReportModule.name,
     myDayReportModule.name,
-    studentModule.name
+    studentModule.name,
+    healthReportModule.name
+
   ])
   .config(function($mdThemingProvider) {
     $mdThemingProvider
@@ -23,3 +27,4 @@ module.exports = angular
       .warnPalette('red')
       .backgroundPalette('grey');
   });
+

yaremakleputs/myproject

app/js/main/main.module.js
@@ -12,7 +12,7 @@ var bottleReportModule = require('.././bottleReport/bottleReport.module.js');
 var sideBarModule = require('./../sidebar/sidebar.module.js');
 var studentService = require('./../common/services/student.service.js');
 var profileModule = require('./../profile/profile.module.js');
-var toggleMessage = require('./../common/services/toggleMessage/toggleMessage.service.js');
+var healthReportModule = require('./../healthReport/healthReport.module.js');
 
 require('../../css/style.scss');
 
@@ -26,12 +26,12 @@ module.exports = angular
     bottleReportModule.name,
     groupService.name,
     toggleMessage.name,
-    staticModule.name,
     sideBarModule.name,
     studentService.name,
     profileModule.name,
     forgotPasswordModule.name,
-    resetPasswordModule.name
+    resetPasswordModule.name,
+    healthReportModule.name
   ])
 
   .config(['$translateProvider', function($translateProvider) {